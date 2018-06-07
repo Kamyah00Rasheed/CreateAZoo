30d916ea0c93589386e62b399ea6da9130c1092e
Kamyah Rasheed <kamyah.rasheed@stu.jefferson.kyschools.us>
2018-06-07 13:37:11 -0400

    Added Rabbit, Rhino, Lion, and Penguin class

---
+++ src/com/company/Penguin.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Penguin extends Animal {
+
+    private String type;
+    private String featherType;
+    private double wingSpan;
+
+
+    public Penguin(String name, String location, char gender, double weight, String type, String featherType, double wingSpan) {
+        super(name, location, gender, weight);
+        this.type = type;
+        this.featherType = featherType;
+        this.wingSpan = wingSpan;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFeatherType() {
+        return featherType;
+    }
+
+    public void setFeatherType(String featherType) {
+        this.featherType = featherType;
+    }
+
+    public double getWingSpan() {
+        return wingSpan;
+    }
+
+    public void setWingSpan(double wingSpan) {
+        this.wingSpan = wingSpan;
+    }
+}
